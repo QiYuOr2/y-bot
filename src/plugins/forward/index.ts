850c033edc7a9bc10bc1f218e4cb18bb0f6b2069
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2022-11-17 11:48:29 +0800

    feat(forward): basic feature

--- src/plugins/forward/index.ts
+++ src/plugins/forward/index.ts
@@ -5,28 +5,24 @@ export class ForwardPlugin extends Plugin {
   constructor() {
     super();
 
-    this.set(['朋友说']).action(() => {
-      return this.toMessage();
-    });
-  }
-
-  toMessage() {
-    return toMessageChain({
-      type: 'Forward',
-      title: '朋友的聊天记录',
-      brief: '聊天记录',
-      summary: '查看 1 条转发消息',
-      source: this.message.source.id.toString(),
-      nodeList: [
-        {
-          senderId: 1045395422,
-          time: parseInt(String(new Date().getTime() / 1000)),
-          senderName: '朋友',
-          messageChain: [
-            Message.Plain('123')
-          ]
-        }
-      ]
+    this.set(['朋友说']).action((message: string) => {
+      return toMessageChain({
+        type: 'Forward',
+        title: '朋友的聊天记录',
+        brief: '聊天记录',
+        summary: '查看 1 条转发消息',
+        source: this.message.source.id.toString(),
+        nodeList: [
+          {
+            senderId: 1176281967,
+            time: parseInt(String(new Date().getTime() / 1000)),
+            senderName: '朋友',
+            messageChain: [
+              Message.Plain(message)
+            ]
+          }
+        ]
+      });
     });
   }
 }
