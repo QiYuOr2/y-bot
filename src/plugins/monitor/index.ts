54659fcee7aa69b8712e1cdfdfbf05a4c5a26433
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2023-02-19 17:46:37 +0800

    feat(monitor): add

--- src/plugins/monitor/index.ts
+++ src/plugins/monitor/index.ts
@@ -17,24 +17,29 @@ async function arknightsInBili(ctx: BotContext) {
 
   await page.goto('https://space.bilibili.com/161775300/dynamic');
 
-  await page.waitForSelector('#page-dynamic > div.col-1 > div > div.bili-dyn-list__items > div:nth-child(2) > div > div > div.bili-dyn-item__body > div.bili-dyn-content > div > div.bili-dyn-content__orig__desc > div > div.bili-rich-text__content', { timeout: 50000 });
+  await page.waitForSelector(
+    '#page-dynamic > div.col-1 > div > div.bili-dyn-list__items > div:nth-child(2) > div > div > div.bili-dyn-item__body > div.bili-dyn-content > div > div.bili-dyn-content__orig__desc > div > div.bili-rich-text__content',
+    { timeout: 50000 }
+  );
 
   const targetContent = await page.evaluate(() => {
     // 注入页面的脚本，无法使用外部变量
-    return document.querySelector('#page-dynamic > div.col-1 > div > div.bili-dyn-list__items > div:nth-child(2) > div > div > div.bili-dyn-item__body > div.bili-dyn-content > div > div.bili-dyn-content__orig__desc > div > div.bili-rich-text__content')?.textContent;
+    return document.querySelector(
+      '#page-dynamic > div.col-1 > div > div.bili-dyn-list__items > div:nth-child(2) > div > div > div.bili-dyn-item__body > div.bili-dyn-content > div > div.bili-dyn-content__orig__desc > div > div.bili-rich-text__content'
+    )?.textContent;
   });
 
   await browser.close();
 
   if (targetContent) {
     if (!isFirstLaunch && cacheContent !== targetContent) {
-      ctx.sendGroup([
+      return [
         '[警告] 监测到世界线变动！\n=====================\n',
         '明日方舟发布了未知的bilibili动态，内容如下: \n\n',
         targetContent,
         '\n=====================\n',
         '详情请前往 https://space.bilibili.com/161775300/dynamic 查看'
-      ].map(t => Message.Plain(t)), 708376391);
+      ].map(t => Message.Plain(t));
     }
     cacheContent = targetContent;
   }
@@ -43,9 +48,14 @@ async function arknightsInBili(ctx: BotContext) {
 }
 
 export const monitor = defineTimer('0 */5 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?',
-  (ctx) => {
+  async (ctx) => {
     try {
-      return arknightsInBili(ctx);
+      const result = await arknightsInBili(ctx);
+      if (result) {
+        // TODO 配置文件
+        ctx.sendGroup(result, 708376391);
+        ctx.sendGroup(result, 398872071);
+      }
     } catch (error) {
       console.log(error);
     }
